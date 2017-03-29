dee0d965561a2c7aa3f4eb52fe7f6e6437927a7d
Wagner Esser <waesser@gmail.com>
2017-03-28 23:58:58 -0300

    Correção de bug no Empréstimo de equipamento.

--- arduino/views/utilizacao.py
+++ arduino/views/utilizacao.py
@@ -1,8 +1,4 @@
 # coding: utf-8
-from django.shortcuts import render, HttpResponseRedirect
-from django.core import urlresolvers
-from django.views.generic import View
-import datetime
 from arduino.models import UtilizacaoModel, EquipamentoModel
 from arduino.views import VisualizarEquipamento
 
@@ -10,22 +6,23 @@ from arduino.views import VisualizarEquipamento
 def Emprestar(request, id_equipamento):
     equipamento = EquipamentoModel.objects.get(pk=id_equipamento)
     usuario = request.user
-    quantidade_a_emprestar = request.POST.get('quantidade')
-    if quantidade_a_emprestar <= equipamento.quantidade_disponivel:
+    qtd_a_emprestar = int(request.POST.get('quantidade'))
+    qtd_disponivel = int(equipamento.quantidade_disponivel)
+    if qtd_a_emprestar <= qtd_disponivel and qtd_a_emprestar > 0:
         utilizacao = UtilizacaoModel(
             equipamento=equipamento,
             usuario=usuario,
-            quantidade_utilizada=quantidade_a_emprestar
+            quantidade_utilizada=qtd_a_emprestar
         )
 
-        equipamento.quantidade_disponivel = int(equipamento.quantidade_disponivel) - int(quantidade_a_emprestar)
+        equipamento.quantidade_disponivel = qtd_disponivel - qtd_a_emprestar
         equipamento.save()
         utilizacao.save()
         msg = "Empréstimo realizado com sucesso."
         cor_msg = "green"
     else:
-        msg="Quantidade inválida."
-        cor_msg="red"
+        msg = "Quantidade inválida."
+        cor_msg = "red"
 
     return VisualizarEquipamento(request, id_equipamento, msg=msg, cor_msg=cor_msg)
 
